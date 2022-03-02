6a527d0b2ee69702e7299f728c6612310e6e6d52
Haoxian Chen <hxchen414@mgmail.com>
2022-03-02 09:33:44 -0500

    [Main] Add regression test.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,22 +6,40 @@ import java.nio.file.Paths
 
 object Main extends App {
   val outDir = "/Users/hxc/projects/smart-contracts/datalog/dsc"
-  if (args(0) == "parse") {
-    val filepath = args(1)
+  val benchmarkDir = "/Users/hxc/projects/declarative-smart-contract/benchmarks"
+  val allBenchmarks = List("auction.dl", "crowFunding.dl", "erc20.dl", "nft.dl", "wallet.dl")
+
+  def run(filepath: String, displayResult: Boolean): Unit = {
     val filename = Misc.getFileNameFromPath(filepath)
     val dl = {
-        val parser = new Parser()
-        val inputStr = Misc.fileToString(filepath)
-        val raw = parser.parseAll(parser.program, inputStr).get
-        val typeChecker = TypeChecker()
-        typeChecker.updateTypes(raw).setName(filename.capitalize)
+      val parser = new Parser()
+      val inputStr = Misc.fileToString(filepath)
+      val raw = parser.parseAll(parser.program, inputStr).get
+      val typeChecker = TypeChecker()
+      typeChecker.updateTypes(raw).setName(filename.capitalize)
     }
-    println(dl)
     val imperative = ImperativeTranslator(dl).translate()
-    println(imperative)
     val solidity = SolidityTranslator(imperative, dl.interfaces,dl.violations).translate()
-    println(s"Solidity program:\n${solidity}")
     val outfile = Paths.get(outDir, s"$filename.sol")
     Misc.writeToFile(solidity.toString, outfile.toString)
+    if (displayResult) {
+      println(dl)
+      println(imperative)
+      println(s"Solidity program:\n${solidity}")
+    }
   }
+
+  if (args(0) == "parse") {
+    val filepath = args(1)
+    run(filepath, displayResult = true)
+  }
+  if (args(0) == "regression-test") {
+    for (p <- allBenchmarks) {
+      println(p)
+      val filepath = Paths.get(benchmarkDir, p).toString
+      run(filepath, displayResult = false)
+    }
+
+  }
+
 }
