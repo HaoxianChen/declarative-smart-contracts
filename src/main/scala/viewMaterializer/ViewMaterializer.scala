0aa89344e6a7a0b3c42648d75049c6296b7823a5
Haoxian Chen <hxchen414@mgmail.com>
2023-05-29 17:19:11 +0800

    [viewMaterializer] Add interface explanation.

--- src/main/scala/viewMaterializer/ViewMaterializer.scala
+++ src/main/scala/viewMaterializer/ViewMaterializer.scala
@@ -4,5 +4,12 @@ import datalog.{Interface, Program, Relation}
 import imp.ImperativeAbstractProgram
 
 abstract class ViewMaterializer {
+  /** Input:
+   * - program: the imperative abstract program,
+   *             where program.rules store the original Datalog rules.
+   * - interfaces: the public interfaces of the smart contracts.
+   *
+   * Output: set of relations to be materialized
+   *  */
   def getMaterializedRelations(program: ImperativeAbstractProgram, interfaces: Set[Interface]): Set[Relation]
 }
